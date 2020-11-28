79731caf9a90d42ceb00b4275074f5ffc9936a9c
Emmanuel Pastor <paros7191@gmail.com>
2020-11-28 20:04:17 +0100

    Add Fetching Utility class
    Allows to easily fetch the TMDb API. Just specify the type of the request, the way you want to update your state and optional query parameters.

---
+++ src/util/FetchUtil.ts
@@ -0,0 +1,33 @@
+
+const API_KEY = process.env.REACT_APP_TMBD_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export enum RequestType {
+    POPULAR_MOVIES = "/movie/popular",
+    UPCOMING_MOVIES = "/movie/upcoming",
+    MOVIE_SEARCH = "/search/movie",
+    MULTI_SEARCH = "/search/multi"
+}
+
+export class Fetcher {
+    private requestUrl: string;
+    private readonly updateState: (it: any) => void;
+
+    constructor(requestType: RequestType, updateState: (it: any) => void) {
+        this.requestUrl = BASE_URL + requestType + "?api_key=" + API_KEY;
+        this.updateState = updateState;
+    }
+
+    addParameter(name: string, value: string): Fetcher {
+        this.requestUrl += "&" + name + "=" + value;
+        return this;
+    }
+
+    async fetch() {
+        const response = await fetch(this.requestUrl);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            this.updateState(jsonResponse);
+        }
+    }
+}
