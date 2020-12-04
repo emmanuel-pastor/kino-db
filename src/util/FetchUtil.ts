e823cf886ac0bdc2e62a7745ef93a6ad69fc9ff3
Emmanuel Pastor <paros7191@gmail.com>
2020-12-04 19:38:37 +0100

    Add fetch requests caching
    Default caching time is 24h

--- src/util/FetchUtil.ts
+++ src/util/FetchUtil.ts
@@ -1,4 +1,3 @@
-
 const API_KEY = process.env.REACT_APP_TMBD_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
@@ -9,12 +8,20 @@ export enum RequestType {
     MULTI_SEARCH = "/search/multi"
 }
 
+interface Cacheable {
+    timestamp: number;
+    content: JSON;
+}
+
 export class Fetcher {
     private requestUrl: string;
+    private readonly requestType: string;
     private readonly updateState: (it: any) => void;
+    private cacheExpirationTime = 1000 * 60 * 60 * 24;
 
     constructor(requestType: RequestType, updateState: (it: any) => void) {
         this.requestUrl = BASE_URL + requestType + "?api_key=" + API_KEY;
+        this.requestType = requestType;
         this.updateState = updateState;
     }
 
@@ -23,13 +30,31 @@ export class Fetcher {
         return this;
     }
 
+    setCacheExpiration(time: number): Fetcher {
+        this.cacheExpirationTime = time;
+        return this;
+    }
+
     async fetch() {
-        const response = await fetch(this.requestUrl);
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            this.updateState(jsonResponse);
+        const cachedValue: any = localStorage.getItem(this.requestType) ? JSON.parse(localStorage.getItem(this.requestType) as string) : "";
+
+        if (cachedValue !== "" && !this.isCacheExpired(cachedValue.timestamp)) {
+            this.updateState(cachedValue.content);
         } else {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            const response = await fetch(this.requestUrl);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                const cache: Cacheable = {timestamp: new Date().getTime(), content: jsonResponse}
+                localStorage.setItem(this.requestType, JSON.stringify(cache));
+                this.updateState(jsonResponse);
+            } else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
         }
+    }
+
+    private isCacheExpired(timestamp: number): boolean {
+        const now = new Date().getTime();
+        return (now - timestamp) > this.cacheExpirationTime;
     }
 }
