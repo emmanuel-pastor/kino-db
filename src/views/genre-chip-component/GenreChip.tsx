32efaf4405d2184353f762f59397ba5f7e916d57
Emmanuel Pastor <paros7191@gmail.com>
2021-01-16 19:03:42 +0100

    Add clickable ability to GenreChip

--- src/views/genre-chip-component/GenreChip.tsx
+++ src/views/genre-chip-component/GenreChip.tsx
@@ -1,20 +1,38 @@
 import {Genre} from "../../domain/DetailedMovie";
 import {ComponentSize} from "../../util/ComponentSize";
 import style from './GenreChip.module.css';
-import React from "react";
+import React, {CSSProperties, useState} from "react";
 
 interface Props {
     genre: Genre,
     size?: ComponentSize
+    onClick?: (genre: Genre) => void
 }
 
-const GenreComponent = (props: Props) => {
+const GenreChip = (props: Props) => {
+    const [isClicked, setClicked] = useState(false);
+
     const componentSize = props.size === undefined ? ComponentSize.MEDIUM : props.size;
-    return(
-        <div key={props.genre.id} className={style.common + ' ' + style[`size-${componentSize}`]}>
-            {props.genre.name}
-        </div>
+    const handleClick = () => {
+        if (props.onClick !== undefined) {
+            setClicked(!isClicked)
+            props.onClick(props.genre)
+        }
+    }
+
+    const defaultStyle = props.onClick!==undefined ? {cursor: "pointer"} as CSSProperties : undefined;
+
+    const clickedStyle = {
+        cursor: "pointer",
+        backgroundColor: "#fe6d8e",
+        color: "white",
+    } as CSSProperties
+
+    return (
+            <div key={props.genre.id} className={style.common + ' ' + style[`size-${componentSize}`]} style={isClicked ? clickedStyle : defaultStyle} onClick={() => handleClick()}>
+                {props.genre.name}
+            </div>
     );
 }
 
-export default GenreComponent;
+export default GenreChip;
